f81cb0907b52b0457a395829a153e06f23eaf1cf
PlankCipher <plankcipher1@protonmail.com>
2021-03-28 03:36:25 +0200

    Add solution for Attribute Parser

--- template/main.cpp
+++ template/main.cpp
@@ -9,8 +9,6 @@ const int MAX_N = 1e5 + 1;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 
-void solve() {}
-
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
@@ -20,6 +18,5 @@ int main() {
   // cin >> tc;
   for (int t = 1; t <= tc; t++) {
     // cout << "Case #" << t << ": ";
-    solve();
   }
 }
