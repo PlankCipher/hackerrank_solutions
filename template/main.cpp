2b1cae2645c66ba222afeefd9ed41b8ea38f5a20
PlankCipher <plankcipher1@protonmail.com>
2021-07-29 02:00:07 +0200

    Add solve function to main template

--- template/main.cpp
+++ template/main.cpp
@@ -9,6 +9,8 @@ const int MAX_N = 1e5 + 1;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 
+// void solve() {}
+
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
@@ -18,5 +20,6 @@ int main() {
   // cin >> tc;
   for (int t = 1; t <= tc; t++) {
     // cout << "Case #" << t << ": ";
+    // solve();
   }
 }
